62eeaf2dd4fcfa2b8f4d4d192ab11bef25443a89
Surya Vivek <suryavivekofficial@gmail.com>
2023-12-30 11:57:41 +0530

    db querying and displaying works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>Hello world</div>;
+import prisma from "@/utils/prisma";
+import { Suspense } from "react";
+
+export default async function Home() {
+  const data = await prisma.todo.findMany();
+  console.log({ data });
+
+  return (
+    <div>
+      <Suspense fallback={<div>loading...</div>}>
+        <ul>
+          {data.map((d) => (
+            <li key={d.id}>{d.text}</li>
+          ))}
+        </ul>
+      </Suspense>
+    </div>
+  );
 }
