d7746c1213be94eb339d00795c446373557d4f66
Surya Vivek <suryavivekofficial@gmail.com>
2024-01-01 18:54:34 +0530

    feat: add new project

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,21 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import prisma from "@/utils/prisma";
+import { revalidatePath } from "next/cache";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import type { FC } from "react";
+import { Label } from "./ui/label";
 
 interface SidebarProps {
   projectId: string;
@@ -8,7 +23,23 @@ interface SidebarProps {
 
 const Sidebar: FC<SidebarProps> = async ({ projectId }) => {
   const projects = await prisma.project.findMany();
-  console.log({ projectId });
+
+  async function createNewProject(formData: FormData) {
+    "use server";
+
+    const project = formData.get("new-project-name") as string;
+
+    console.log("server actions: ", project);
+
+    const data = await prisma.project.create({
+      data: {
+        name: project,
+      },
+    });
+
+    revalidatePath(`/${data.id}`);
+    redirect(`/${data.id}`);
+  }
 
   return (
     <div className="w-1/5 py-4 px-6 divide-y space-y-8">
@@ -25,10 +56,44 @@ const Sidebar: FC<SidebarProps> = async ({ projectId }) => {
           </Link>
         ))}
       </div>
-      <button className="py-2 text-sm border-t border-zinc-400 w-full text-blue-800 px-4 flex items-center justify-center duration-200 hover:border-blue-100 hover:rounded-md hover:bg-blue-100">
-        + Add New Project
-      </button>
-      {/* <ProjectInput /> */}
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="ghost" className="text-indigo-900">
+            + Add New Project
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add New Project</DialogTitle>
+            <DialogDescription>
+              Add a new project to track your progress.
+            </DialogDescription>
+          </DialogHeader>
+          <form action={createNewProject} className="space-y-2">
+            <Label htmlFor="new-project-name">Project Name</Label>
+            <Input id="new-project-name" name="new-project-name" />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="bg-blue-50 text-blue-700"
+                >
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button
+                type="submit"
+                variant="default"
+                className="bg-blue-600 text-zinc-50 hover:bg-blue-700"
+              >
+                Add
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
