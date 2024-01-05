5ccf52bd3c951685cfbdf18ee8de7c1f7b76ca7c
Surya Vivek <suryavivekofficial@gmail.com>
2024-01-05 19:56:58 +0530

    fix: loading UI - spinner

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -21,6 +21,7 @@ import {
   type FormEvent,
   type SetStateAction,
 } from "react";
+import Loading from "./Loading";
 
 type TodoStatus = "none" | "Progress" | "Review" | "Completed";
 
@@ -230,28 +231,7 @@ const TodoForm: FC<TodoFormProps> = ({
             className="space-x-2 bg-blue-600 text-zinc-50 hover:bg-blue-700"
           >
             <span>Add</span>
-            {loading && (
-              <svg
-                className="animate-spin h-4 w-4 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  stroke-width="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            )}
+            {loading && <Loading />}
           </Button>
         </div>
       </div>
